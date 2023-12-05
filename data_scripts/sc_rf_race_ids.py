26c6a3e1fdfc86976747b92a6d7daf4c24a216c4
Alex Manolache <a.d.manolache@student.tudelft.nl>
2023-12-05 16:35:29 +0100

    Stacked barchart for SC and RF

--- data_scripts/sc_rf_race_ids.py
+++ data_scripts/sc_rf_race_ids.py
@@ -25,11 +25,19 @@ def add_race_ids(file_path, save_path):
                 print(f"Race ID for '{race_name}' not found.")
                 exit(1)
             
+            if race_id in race_ratings_dict.keys():
+                rating = race_ratings_dict[race_id]
+            else:
+                print(f"No race rating for '{(race_id, race_name)}'.")
+                rating = None
+            
             row['year'] = year
+            row['race_name'] = race_name
+            row['rating'] = rating
             new_rows.append(row)
 
     with open(save_path, 'w', newline='') as file:
-        fieldnames = reader.fieldnames + ['raceId', 'year']
+        fieldnames = reader.fieldnames + ['raceId', 'year', 'race_name', 'rating']
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
         for row in new_rows:
@@ -43,6 +51,15 @@ with open('../data/races.csv', 'r') as races_file:
         race_name = row['name']
         race_id = row['raceId']
         race_info_dict[(year, race_name)] = race_id
+
+race_ratings_dict = {}
+with open("../data/race_ratings_with_race_ids.csv", 'r') as ratings_file:
+    reader = csv.DictReader(ratings_file)
+    for row in reader:
+        race_id = row['raceId']
+        rating = row['RATING']
+        race_ratings_dict[race_id] = rating
+
 
 add_race_ids('../data/safety_cars.csv', '../data/safety_cars_with_race_ids.csv')
 add_race_ids('../data/red_flags.csv', '../data/red_flags_with_race_ids.csv')
